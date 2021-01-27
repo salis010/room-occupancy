ecbfa5e62f73ab71cd9454c6937bb56def88bfed
Stephen <stephen.saliba@gmail.com>
2021-01-27 21:10:05 +0100

    Completes state management

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { H1, H2, PageWrapper, RoomTypesWrapper } from './common'
 import { RoomType } from './room-type'
-import { getUsage } from './utils/get-usage'
+import { getUsage } from '../utils/get-usage'
 
 export const App = () => {
+  const [ bids, setBids ] = useState([])
   const [ freeRooms, setFreeRooms ] = useState({
     economy: 0,
     premium: 0
@@ -19,14 +20,23 @@ export const App = () => {
     } 
   })
 
+  useEffect(() => {
+    fetch("https://gist.githubusercontent.com/lwhiteley/b01cf0964e19704df06fccf44d0c3c4d/raw/580a0aa9675985674dd1a70ffa799a4288c94bb3/guests.json")
+    .then(response => response.json())
+    .then(data => setBids(data))
+    .catch(err => console.log("Failed to get JSON file: ", err))
+  }, [])
+  
   const onChangeHandler = event => setFreeRooms(
     {
       ...freeRooms, 
       [event.target.id]: event.target.value
     }
   )
-
-  const onClickHandler = () => setUsage(getUsage(usage.premium))
+    
+  const onClickHandler = () => {
+    setUsage(getUsage(freeRooms.premium, freeRooms.economy, bids))
+  }
 
   return (
     <PageWrapper>
@@ -36,6 +46,11 @@ export const App = () => {
         <RoomType id="premium" roomType="Premium" value={freeRooms.premium} onChange={onChangeHandler} />
         <RoomType id="economy" roomType="Economy" value={freeRooms.economy} onChange={onChangeHandler} />
       </RoomTypesWrapper>
+      <button onClick={onClickHandler}>Check Occupancy</button>
+      <p>{usage.premium.usage}</p>
+      <p>{usage.premium.revenue}</p>
+      <p>{usage.economy.usage}</p>
+      <p>{usage.economy.revenue}</p>
     </PageWrapper>
   )
 }
