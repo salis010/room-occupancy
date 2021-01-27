fa47f2514e20b63c99bdd6b6a7b8ddecf2da9fd1
Stephen <stephen.saliba@gmail.com>
2021-01-27 20:17:50 +0100

    Adds common components

---
+++ src/components/common.jsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+
+export const PageWrapper = styled.div`
+    margin: 3rem;
+`
+
+export const H1 = styled.h1`
+    font-size: 3rem;
+`
+
+export const H2 = styled.h2`
+    font-size: 2rem;
+`
+
+export const RoomTypesWrapper = styled.div`
+    display: flex;
+`
+
+export const Label = styled.label`
+    font-size: 1.4rem;
+`
