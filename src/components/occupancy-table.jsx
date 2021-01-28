155469292235ec1c5cbb3e798eddba5500218627
Stephen <stephen.saliba@gmail.com>
2021-01-28 15:36:22 +0100

    Includes table totals

--- src/components/occupancy-table.jsx
+++ src/components/occupancy-table.jsx
@@ -3,29 +3,39 @@ import PropTypes from 'prop-types'
 
 export const OccupancyTable = ({
   premiumUsage, premiumRevenue, economyUsage, economyRevenue,
-}) => (
-  <table>
-    <thead>
-      <tr>
-        <td />
-        <th>Premium</th>
-        <th>Economy</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Usage</td>
-        <td>{premiumUsage}</td>
-        <td>{premiumRevenue}</td>
-      </tr>
-      <tr>
-        <td>Revenue</td>
-        <td>{economyUsage}</td>
-        <td>{economyRevenue}</td>
-      </tr>
-    </tbody>
-  </table>
-)
+}) => {
+  const totalRevenue = premiumRevenue + economyRevenue
+  const totalUsage = premiumUsage + premiumUsage
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td />
+          <th>Usage</th>
+          <th>Revenue</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Premium</td>
+          <td>{premiumUsage}</td>
+          <td>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(premiumRevenue)}</td>
+        </tr>
+        <tr>
+          <td>Economy</td>
+          <td>{economyUsage}</td>
+          <td>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(economyRevenue)}</td>
+        </tr>
+        <tr>
+          <td>Totals</td>
+          <td>{totalUsage}</td>
+          <td>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(totalRevenue)}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 OccupancyTable.propTypes = {
   premiumUsage: PropTypes.number.isRequired,
