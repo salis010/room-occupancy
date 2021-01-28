58ac9f2626149200b1d58faa3c539ddc2c00c835
Stephen <stephen.saliba@gmail.com>
2021-01-28 20:35:43 +0100

    Adds style to table

--- src/components/occupancy-table.jsx
+++ src/components/occupancy-table.jsx
@@ -1,5 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+const TableWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 1rem;
+    border: 1px solid ${(props) => props.theme.colors.lightGrey};
+    border-radius: ${(props) => props.theme.dimensions.borderRadius};
+`
+
+const Table = styled.table`
+    font-size: 1.2rem;
+    width: 35rem;
+    padding: 3rem 2.4rem;
+    border-collapse: collapse;
+    color: ${(props) => props.theme.colors.darkGrey};
+`
+
+const TH = styled.th`
+    font-weight: 600;
+    padding: 1rem;
+    border-bottom: 1px solid ${(props) => props.theme.colors.lightGrey};
+    
+`
+
+const TD = styled.td`
+    padding: 1rem;
+    text-align: center;
+
+    &:nth-of-type(1) {
+        text-align: left;
+    }
+`
+
+const TR = styled.tr`
+    &:nth-of-type(3) {
+        font-weight: 800;
+        border-top: 1px solid ${(props) => props.theme.colors.lightGrey};
+    }
+`
 
 export const OccupancyTable = ({
   premiumUsage, premiumRevenue, economyUsage, economyRevenue,
@@ -8,32 +49,34 @@ export const OccupancyTable = ({
   const totalUsage = premiumUsage + premiumUsage
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <td />
-          <th>Usage</th>
-          <th>Revenue</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Premium</td>
-          <td>{premiumUsage}</td>
-          <td>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(premiumRevenue)}</td>
-        </tr>
-        <tr>
-          <td>Economy</td>
-          <td>{economyUsage}</td>
-          <td>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(economyRevenue)}</td>
-        </tr>
-        <tr>
-          <td>Totals</td>
-          <td>{totalUsage}</td>
-          <td>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(totalRevenue)}</td>
-        </tr>
-      </tbody>
-    </table>
+    <TableWrapper>
+      <Table>
+        <thead>
+          <TR>
+            <TH />
+            <TH>Usage</TH>
+            <TH>Revenue</TH>
+          </TR>
+        </thead>
+        <tbody>
+          <TR>
+            <TD>Premium</TD>
+            <TD>{premiumUsage}</TD>
+            <TD>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(premiumRevenue)}</TD>
+          </TR>
+          <TR>
+            <TD>Economy</TD>
+            <TD>{economyUsage}</TD>
+            <TD>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(economyRevenue)}</TD>
+          </TR>
+          <TR>
+            <TD>Totals</TD>
+            <TD>{totalUsage}</TD>
+            <TD>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0 }).format(totalRevenue)}</TD>
+          </TR>
+        </tbody>
+      </Table>
+    </TableWrapper>
   )
 }
 
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,11 @@
 export const theme = {
   colors: {
     white: 'RGB(250, 250, 250)',
-    primary: 'RGB(100, 150, 150)',
+    primary: '#331fb4',
+    secondary: '#b657d3',
     text: 'RGB(50, 50, 50)',
+    darkGrey: 'RGB(100, 100, 100)',
+    lightGrey: 'RGB(200, 200, 200)',
   },
   dimensions: {
     radius: '20px',
